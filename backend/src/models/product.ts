3cce2a5d456d7ec3ea9ae38fc7ee240960e66888
Yair Shavit <y.shavit.mail+git@gmail.com>
2026-07-08 20:19:34 +0300

    Add product interface

---
+++ backend/src/models/product.ts
@@ -0,0 +1,17 @@
+export interface Product {
+    id: number;
+    name: string;
+    room_id: number;
+    category_id: number;
+    in_stock: number;
+    minimum_in_stock: number;
+    is_expendable: boolean;
+    weight?: number;
+    picture_url?: string;
+    deleted_at?: Date;
+    room_name?: string;
+    category_name?: string;
+}
+
+export type CreateProductInput = Omit<Product, 'id' | 'room_name' | 'category_name' | 'deleted_at'>;
+export type UpdateProductInput = Partial<Omit<Product, 'id' | 'room_name' | 'category_name' | 'deleted_at'>>;
